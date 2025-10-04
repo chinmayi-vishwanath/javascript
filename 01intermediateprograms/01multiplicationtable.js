1403504560ba0f17af64f9b7a3e9b70e5300b465
chinmayi <vchinmayi1@gmail.com>
2025-10-04 09:06:40 +0530

    intermediateprograms

---
+++ 01intermediateprograms/01multiplicationtable.js
@@ -0,0 +1,8 @@
+// Multiplication Table 
+let n=10;
+function mul(num){
+    for(let i=1;i<n;i++){
+        console.log(`${num}*${i} =${num*i}`)
+    }
+}
+mul(2)
---
+++ 01intermediateprograms/basic.md
@@ -0,0 +1,6 @@
+# this folder contains the intermediate programs
+
+1. Multiplication table
+2. Factorial using recursion
+3.  Palindrome checker
+4. Fiboniccai
